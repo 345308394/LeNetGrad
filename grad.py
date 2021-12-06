0ac25c46400cdd0a0bd02f563a017be80e0e7abb
wangzhaoxin <wangzhaoxin@cnu.edu.cn>
2021-12-06 19:05:51 +0800

    update

--- grad.py
+++ grad.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from Second_order_gradients import get_second_order_grad
+from second_order_gradients import get_second_order_grad
 from model import Model
 import numpy as np
 import torch
